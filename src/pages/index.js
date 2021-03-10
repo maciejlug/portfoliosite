49456c49bfa805c9ee47d73ec943200f4d5e29de
maciejlug <maciej.lugowski5@gmail.com>
2021-03-10 18:33:09 +0100

    hotfix4

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,19 +15,37 @@ class App extends React.Component {
 
       fullpages: [
         {
-          text: 'Section',
+          text: 'Section 1',
           content: <HeaderContent/>
         },
         {
-          text: 'Section',
+          text: 'Section 2',
           content: <ProjectsContent/>
         },
         {
-          text: 'Section',
+          text: 'Section 3',
           content: <ContactContent/>
         }
       ],
     };
+  }
+
+  onLeave(origin, destination, direction) {
+   if(destination.index===0){
+      document.getElementsByClassName('navbar-link')[1].classList.remove('on');
+      document.getElementsByClassName('navbar-link')[2].classList.remove('on');
+      document.getElementsByClassName('navbar-link')[0].classList.add('on');
+    }
+    if(destination.index===1){
+      document.getElementsByClassName('navbar-link')[0].classList.remove('on');
+      document.getElementsByClassName('navbar-link')[2].classList.remove('on');
+      document.getElementsByClassName('navbar-link')[1].classList.add('on');
+    }
+    if(destination.index===2){
+      document.getElementsByClassName('navbar-link')[0].classList.remove('on');
+      document.getElementsByClassName('navbar-link')[1].classList.remove('on');
+      document.getElementsByClassName('navbar-link')[2].classList.add('on');
+    }
   }
 
   render() {
@@ -43,7 +61,7 @@ class App extends React.Component {
       <div className="App">
        
         
-        <Navbar/>
+       <Navbar />
         <ReactFullpage
           debug /* Debug logging */
 
@@ -51,6 +69,7 @@ class App extends React.Component {
           licenseKey={'YOUR_KEY_HERE'} // Get one from https://alvarotrigo.com/fullPage/pricing/
           navigation
           anchors={['firstPage', 'secondPage', 'thirdPage']}
+          onLeave={this.onLeave.bind(this)}
           sectionSelector={SECTION_SEL}
 
           render={comp => (
