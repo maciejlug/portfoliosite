732c98bd72b6019dbb754f453f1d1e59c1ef4bc0
maciejlug <maciej.lugowski5@gmail.com>
2021-03-11 19:13:52 +0100

    hotfix6

--- src/pages/components/ProjectsContent.js
+++ src/pages/components/ProjectsContent.js
@@ -34,9 +34,9 @@ const ProjectsContent = () => {
                                     <img src={PortfolioImg} alt="project1"></img>
                                 </div>
                                 <div className="projects-text">
-                                    <h1>Snake Game</h1>
-                                    <p>Game written in JavaScript. You can play solo or
-                                        with your friend in 2 players mode.</p>
+                                    <h1>Portfolio</h1>
+                                    <p>The site we currently are on. To make it I
+                                    used Gatsby framework and fullPage.js.</p>
                                     <div className="info-links">
                                         <a href="https://snakeportfolio.netlify.app">Live</a>
                                         <a href="https://github.com/maciejlug/snakegame">Github</a>
@@ -52,12 +52,12 @@ const ProjectsContent = () => {
                                     <img src={GalleryImg} alt="project1"></img>
                                 </div>
                                 <div className="projects-text">
-                                    <h1>Snake Game</h1>
-                                    <p>Game written in JavaScript. You can play solo or
-                                        with your friend in 2 players mode.</p>
+                                    <h1>Gallery</h1>
+                                    <p>This site is written in pure JavaScript. The purpose
+                                    of it was to learn.</p>                                   
                                     <div className="info-links">
-                                        <a href="https://snakeportfolio.netlify.app">Live</a>
-                                        <a href="https://github.com/maciejlug/snakegame">Github</a>
+                                        <a href="https://mygalleryjs.netlify.app">Live</a>
+                                        <a href="https://github.com/maciejlug/gallery-js-css">Github</a>
                                     </div>
                                 </div>
                             </div>
